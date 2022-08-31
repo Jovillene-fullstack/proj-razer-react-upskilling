3375bf2cd74235afd6e5d18d5962e654d130a593
Jovillene-fullstack <jovillene@fullstack.ph>
2022-08-31 17:35:35 +0800

    UPDATE

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <input type="text" />
       <button>Add Todo</button>
       <button>Clear Complete</button>
-      <div> 0 left to do </div>
+      <div> 0 LEFT TO DO </div>
     </>
   );
 }
