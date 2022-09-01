6541d4cdb4181c761463b9ab7c2b1eda096761b6
Jovillene-fullstack <jovillene@fullstack.ph>
2022-09-01 15:06:57 +0800

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,95 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import logo from "../assets/logo.png";
+
+function OffcanvasExample() {
+  return (
+    <>
+      {["xxl"].map((expand) => (
+        <Navbar key={expand} expand={expand} className="mb-3">
+          <Container>
+            <Navbar.Brand href="#">
+              <img src={logo} alt="" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <NavDropdown
+                    title="Product"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action1">laptops</NavDropdown.Item>
+                    <NavDropdown.Item href="#action2">mobiles</NavDropdown.Item>
+                    <NavDropdown.Item href="#action2">
+                      mouse & mats
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action2">
+                      keyboards
+                    </NavDropdown.Item>
+
+                    <NavDropdown
+                      title="Headset & audio"
+                      id={`offcanvasNavbarDropdown-expand-${expand}`}
+                    >
+                      <div className="sub2">
+                        <NavDropdown.Item href="#action1">
+                          wireless
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action2">
+                          wired
+                        </NavDropdown.Item>
+                      </div>
+                    </NavDropdown>
+
+                    {/* <DropdownButton
+                      as={ButtonGroup}
+                      id={`dropdown-button-drop-right`}
+                      // drop={direction}
+                      variant="secondary"
+                      title={` Drop end `}
+                    >
+                      <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                      <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                      <Dropdown.Item eventKey="3">
+                        Something else here
+                      </Dropdown.Item>
+                      <Dropdown.Divider />
+                      <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                    </DropdownButton> */}
+                    <NavDropdown.Item href="#action2">console</NavDropdown.Item>
+
+                    <NavDropdown.Item href="#action3">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="#action4">about</Nav.Link>
+                  <Nav.Link href="#action5">community</Nav.Link>
+                  <Nav.Link href="#action6">contact us</Nav.Link>
+                </Nav>
+
+                <Button className="g-btn">buy now</Button>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default OffcanvasExample;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import thx from "../assets/razer-nari-thx-logo.png";
+import chroma from "../assets/razer-nari-chroma-logo.png";
+import razer from "../assets/razer-nari-logo.svg";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Hero = () => {
+  return (
+    <div id="hero">
+      <Container>
+        <Row>
+          <Col className="info">
+            <h1>SUPREME WIRELESS IMMERSION</h1>
+            <p>
+              When it comes to gaming immersion, sound plays a critical role. It
+              engages your sense of hearing to determine the atmosphere and
+              mood, creating a lifelike experience that lets you truly embody
+              your game character. Standing by this philosophy, we’ve built a
+              headset with THX Spatial Audio to bring you 360° positional sound,
+              giving you greater spatial awareness. We’ve also added ergonomic
+              design features to make this one of the most comfortable daily
+              drivers for gaming. This is the Razer Nari.
+            </p>
+
+            <img src={thx} alt="thx" />
+            <img src={chroma} alt="thx" />
+          </Col>
+          <Col className="svg">
+            <img className="razer-svg" src={razer} alt="" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
