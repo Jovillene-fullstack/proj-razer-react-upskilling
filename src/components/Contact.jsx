e8495e0a9dc649a038a01c2d7bbafce6de5e4b62
Jovillene-fullstack <jovillene@fullstack.ph>
2022-09-06 13:33:46 +0800

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,46 @@ import Col from "react-bootstrap/Col";
 import validator from "validator";
 
 function Contact() {
+  function validate() {
+    var name = document.getElementById("name").value;
+
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var add = document.getElementById("add").value;
+    var msg = document.getElementById("msg").value;
+    var error_message = document.getElementById("error_message");
+
+    error_message.style.padding = "10px";
+
+    var text;
+    if (name.length < 5) {
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if (add.length < 10) {
+      text = "Please Enter Correct Address";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if (isNaN(phone) || phone.length != 11) {
+      text = "Please Enter valid Phone Number";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if (email.indexOf("@") == -1 || email.length < 6) {
+      text = "Please Enter valid Email";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if (msg.length <= 5) {
+      text = "Please Enter More Than 140 Characters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+  }
   return (
     <div id="contact">
       <Container>
@@ -11,8 +51,8 @@ function Contact() {
           <Col lg={5}>
             <div className="con-header">
               <h4>ask a question</h4>
-
-              <form>
+              <div id="error_message"></div>
+              <form id="myform" onSubmit={validate}>
                 <input type="text" id="name" placeholder="Name" />
                 <input type="text" id="phone" placeholder="Phone Number" />
                 <input type="text" id="email" placeholder="Email Address" />
